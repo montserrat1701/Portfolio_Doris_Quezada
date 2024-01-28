239ab378c29cadf20cc2773b54e66f8b8fd7d869
Doris Quezada <109364677+montserrat1701@users.noreply.github.com>
2024-01-27 19:11:45 -0700

    Modificacion about bd commit

--- database/migrations/2024_01_09_075243_create_projects_table.php
+++ database/migrations/2024_01_09_075243_create_projects_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('skill_id')->cascadeOnDelete();
             $table->string('name');
+            $table->string('description');
             $table->string('image');
             $table->string('project_url')->nullable();
             $table->timestamps();
--- database/migrations/2024_01_24_035516_create_abouts_table.php
+++ database/migrations/2024_01_24_035516_create_abouts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('abouts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('project_id')->constrained()->cascadeOnDelete();
+            $table->string('text');
+            $table->string('description');
+            $table->string('image');
             $table->timestamps();
         });
     }
