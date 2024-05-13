e8bb2290fe341da37ef6f6248644df24f92e8e2c
Doris Quezada <domoquezadaga@ittepic.edu.mx>
2024-05-13 10:21:40 -0700

    commit final 2.3

--- resources/js/Components/Frontend/Aboutbd.vue
+++ resources/js/Components/Frontend/Aboutbd.vue
@@ -13,11 +13,13 @@
             y: 0
         }">
         <div class="flex flex-col xl:flex-row gap-24">
-            <template v-for="about in abouts.data" :key="about.id">
-                <div v-if="about.text === 'title 2'">
-                    <img class="object-cover w-full h-full md:mx-auto lg:mx-0 rounded-2xl" :src="about.image" :alt="about.name"/>
-                </div>
-            </template>
+            <div class="h-64">
+                <template v-for="about in abouts.data" :key="about.id">
+                    <div v-if="about.text === 'title 2'">
+                        <img class="object-cover w-full  md:mx-auto lg:mx-0 rounded-2xl" :src="about.image" :alt="about.name"/>
+                    </div>
+                </template>
+            </div>
             <div class="flex flex-col items-center text-center lg:items-start lg:text-left">
                 <div class="flex flex-col">
                     <template v-for="about in abouts.data" :key="about.id">
