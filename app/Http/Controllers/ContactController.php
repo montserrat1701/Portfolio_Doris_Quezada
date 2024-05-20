a9befd156ee4ea15da18f5ce3efc6de758dbce52
Doris Quezada <domoquezadaga@ittepic.edu.mx>
2024-05-20 09:15:37 -0700

    cambios en controller de contac

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ContactRequest;
 use App\Mail\ContactMail;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
